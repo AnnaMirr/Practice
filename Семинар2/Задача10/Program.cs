fe24d8ad3b26e46c9d6e8cb4de46e7da82152f51
AnnaSmir <heretto@yandex.ru>
2022-09-21 20:31:59 +0300

    Initial commit

---
+++ Семинар2/Задача10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Enter tree-digit number: ");
+string stringNumber = Convert.ToString(Console.ReadLine());
+
+Console.WriteLine("Second number is "+stringNumber[1]);
