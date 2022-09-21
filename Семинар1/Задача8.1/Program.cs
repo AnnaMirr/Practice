0654f4bb48d1f1309c72e2443ccfea7b625d31d5
AnnaSmir <heretto@yandex.ru>
2022-09-21 17:38:36 +0300

    Initial commit

---
+++ Семинар1/Задача8.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая на вход принимает число (N) , а на выходе показывает все четные числа от 1-N
+
+Console.Clear();
+
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+int number = 2;
+
+while (number <= num)
+{
+    if (number % 2 == 0) Console.Write(number + ", ");
+    number += 2;
+}
