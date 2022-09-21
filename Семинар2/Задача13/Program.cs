c56aaeb6fb243e0f537e71fbcff618ec2a4198a3
AnnaSmir <heretto@yandex.ru>
2022-09-21 21:45:48 +0300

    Initial commit

---
+++ Семинар2/Задача13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.Clear();
+
+
+Console.WriteLine("Enter tree-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+
+
+
+if (stringNumber.Length > 2)
+{
+    Console.WriteLine("Third number is "+stringNumber[2]);
+}
+else{
+    Console.WriteLine("Third number doesn't exist.");
+}
