4cb8073e099ee69d90683f374a6498a9d183558d
AnnaSmir <heretto@yandex.ru>
2022-09-28 18:33:31 +0300

    Initial commit

--- Expl/Program.cs
+++ Expl/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine(" 2 Hello, World!");
+﻿int power(int baseNum, int deg)
+{
+    int result = 1;
+
+    for (int i = 0; i < deg; i++)
+        result *= baseNum;
+
+    return result;
+}
+
+
+int number = 0;
+int degree = 0;
+
+Console.Write("Input A: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+while (true)
+{
+    Console.Write("Input B: ");
+    degree = Convert.ToInt32(Console.ReadLine());
+    if (degree > 0)
+        break;
+    else
+        Console.WriteLine("The degree must be more then 0");
+}
+
+
+Console.WriteLine(power(number, degree));
