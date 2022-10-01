b14de797af22403bd29d613cfd6462b4c0b0d484
AnnaSmir <heretto@yandex.ru>
2022-10-01 22:26:35 +0300

    Initial commit

---
+++ Семинар4/Задача25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+int power(int baseNum, int deg)
+{
+    int result = 1;
+
+    for (int i = 0; i < deg; i++)
+        result *= baseNum;
+
+    return result;
+}
+
+Console.WriteLine("Enter number: ");
+int baseNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the degree for your number: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(power(baseNum, deg));
