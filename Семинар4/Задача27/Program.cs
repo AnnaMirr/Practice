e4be839afcb61fcc8f21bbd1549550ffe7b6eae8
AnnaSmir <heretto@yandex.ru>
2022-10-02 15:34:51 +0300

    Initial commit

---
+++ Семинар4/Задача27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+
+int SumNumber(string number, int counter)
+{
+
+    int numberN = Convert.ToInt32(number);
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        result = result + numberN % 10;
+        numberN = numberN / 10;
+    }
+    return result;
+}
+
+Console.Write("Enter number: ");
+string numberN = Console.ReadLine() ?? "0";
+
+int sumNumber = SumNumber(numberN, numberN.Length);
+Console.WriteLine("The sum of the digits of your number: " + sumNumber);
