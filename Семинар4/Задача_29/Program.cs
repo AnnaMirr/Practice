ef1a27085ab54c0699e08d9015678544d707fcfe
AnnaSmir <heretto@yandex.ru>
2022-10-02 23:43:23 +0300

    Initial commit

---
+++ Семинар4/Задача_29/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+void FillArray(int[] numbers)
+{
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(0, 100);  // можно оставить пустые скобки или поменять диапозон
+    }
+
+}
+
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.WriteLine("[{0}]", string.Join(", ", numbers));
+    }
+}
+
+
+void Zadacha29()
+{
+    int[] numbers = new int[];
+    FillArray(numbers);
+    PrintArray(numbers);
+}
+
+
+Zadacha29();
