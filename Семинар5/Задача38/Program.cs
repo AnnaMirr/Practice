413e210b66bdfc60b5fc19498330c3615a1b3acd
AnnaSmir <heretto@yandex.ru>
2022-10-07 22:27:39 +0300

    Initial commit2

--- Семинар5/Задача38/Program.cs
+++ Семинар5/Задача38/Program.cs
@@ -14,7 +14,8 @@ void FillArrayRandomNumbers(double[] numbers)
     Random random = new Random();
     for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = random.NextDouble();
+        // numbers[i] = random.NextDouble() *1000;
+        numbers[i] = Convert.ToDouble(random.Next(-100,100)/10.0);
     }
 }
 
@@ -23,7 +24,7 @@ void PrintArray(double[] numbers)
     Console.Write("[ ");
     for (int i = 0; i < numbers.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        Console.Write(Math.Round(numbers[i], 2, MidpointRounding.AwayFromZero) + " ");
     }
     Console.Write("]");
     Console.WriteLine();
