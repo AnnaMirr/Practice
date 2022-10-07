ca396b364abf934e539f2192bd91932c9ffc1c89
AnnaSmir <heretto@yandex.ru>
2022-10-07 23:00:38 +0300

    Initial commit3

--- Семинар5/Задача38/Program.cs
+++ Семинар5/Задача38/Program.cs
@@ -6,8 +6,22 @@ double[] numbers = new double[5];
 
 FillArrayRandomNumbers(numbers);
 PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
 
-
+for (int z = 0; z < numbers.Length; z++)
+{
+    if (numbers[z] > max)
+    {
+        max = numbers[z];
+    }
+    if (numbers[z] < min)
+    {
+        min = numbers[z];
+    }
+}
+Console.WriteLine($"Max:{max}, Min: {min}");
+Console.WriteLine($"The difference between the max and min numbers: {max - min}");
 
 void FillArrayRandomNumbers(double[] numbers)
 {
@@ -15,7 +29,7 @@ void FillArrayRandomNumbers(double[] numbers)
     for (int i = 0; i < numbers.Length; i++)
     {
         // numbers[i] = random.NextDouble() *1000;
-        numbers[i] = Convert.ToDouble(random.Next(-100,100)/10.0);
+        numbers[i] = Convert.ToDouble(random.Next(-100, 100) / 10.0);
     }
 }
 
