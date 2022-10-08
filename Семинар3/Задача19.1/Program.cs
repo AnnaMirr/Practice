9a5f07a5598d6c535fc666ca02813b7b1c33695a
AnnaSmir <heretto@yandex.ru>
2022-10-08 16:03:35 +0300

    Initial commit

---
+++ Семинар3/Задача19.1/Program.cs
@@ -0,0 +1,24 @@
+﻿// число 5 знаков папалиндром (2ой вариант)
+
+void Palindrom()
+{
+    int number = 123321;
+    int result = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        result = result * 10 + digit;
+        number /= 10;
+        Console.WriteLine(result);
+    }
+    if (number == result)
+    {
+        Console.WriteLine($"Number {number} is a palindrom.");
+    }
+    else 
+    {
+        Console.WriteLine($"Number {number} isn't a palindrom.");
+    }
+
+}
+Palindrom();
