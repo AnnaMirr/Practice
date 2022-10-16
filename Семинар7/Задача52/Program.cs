9b5a3cb3b9b993c51caf207fbd57430cb1659ff9
AnnaSmir <heretto@yandex.ru>
2022-10-16 23:28:34 +0300

    Initial commit

---
+++ Семинар7/Задача52/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+
+int[,] numbers = new int[3, 5];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+double[] avgNumbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result += numbers[j, i];
+    }
+    avgNumbers[i] = result / numbers.GetLength(0);
+}
+PrintArrayResult(avgNumbers);
+
+
+void FillArrayRandomNumbers(int[,] numbers)
+{
+    Random random = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = random.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(numbers[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+     }
+}
+
+void PrintArrayResult(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
