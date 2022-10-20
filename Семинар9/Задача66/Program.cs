58941d5aeb8018ff0433fa6190f4deffe55dbc42
AnnaSmir <heretto@yandex.ru>
2022-10-20 19:28:55 +0300

    Initial commit

---
+++ Семинар9/Задача66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Рекурсивно
+
+Console.WriteLine("Enter natural number A:");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Enter natural number B:");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+
+void GapNumberSum(int numberA, int numberB, int sum)
+{
+    if (numberA > numberB)
+    {
+        Console.WriteLine($"The sum of elements from A to B: {sum}");
+        return;
+    }
+    sum = sum + (numberA++);
+    GapNumberSum(numberA, numberB, sum);
+}
+
+GapNumberSum(numberA, numberB, 0);
